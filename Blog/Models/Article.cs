3ad21d844aec83e64c91d54ceb8ba5aea0fd5376
DimitaBlazhev <shub@abv.bg>
2016-12-15 21:33:04 +0200

    csharpblogdesign

---
+++ Blog/Models/Article.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Blog.Models
+{
+    public class Article
+    {
+     
+        private ICollection<Tag> tags;
+
+        public Article()
+        {
+            this.tags = new HashSet<Tag>();
+        }
+
+        public Article(string authorId, string tilte, string content,int categoryId)
+        {
+            this.AuthorId = authorId;
+            this.Title = tilte;
+            this.Content = content;
+            this.CategoryId = categoryId;
+            this.tags = new HashSet<Tag>();
+
+        }
+
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [ForeignKey("Author")]
+        public string AuthorId { get; set; }
+
+        public virtual ApplicationUser Author { get; set; }
+
+        public bool IsAuthor(string name)
+        {
+            return this.Author.UserName.Equals(name);
+        }
+        public virtual ICollection<Tag> Tags
+        {
+            get { return this.tags; }
+            set { this.tags = value; }
+        }
+
+    }
+
+    public class Category
+    {
+        private ICollection<Article> articles;
+
+        public Category()
+        {
+            this.articles = new HashSet<Article>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Index(IsUnique = true)]
+        [StringLength(20)]
+        public string Name { get; set; }
+
+        public virtual ICollection<Article> Articles { get; set; }
+
+     
+    }
+}
